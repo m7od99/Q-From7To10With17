fc18816a23bb55d0c6607f91bd11316392cd95e0
cal <m7od99@icloud.com>
2020-12-09 20:43:10 +0300

    Add encoder class with steps

---
+++ Encoder.cs
@@ -0,0 +1,10 @@
+namespace newQuestions
+{
+    public class Encoder
+    {
+        //0- a method Encoder for take string them turn into a secret code .
+        
+
+        //0- a method Decoder to take a secrrt code and recover the string .
+    }
+}
