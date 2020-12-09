55827a31b4717d04794fc71f02a5b9c101147d51
cal <m7od99@icloud.com>
2020-12-09 19:13:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace newQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
